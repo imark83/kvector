b785d7049e2cbea7b6aab58789aa580439eee04a
Marcos Rodríguez <marcos@unizar.es>
2018-06-04 09:57:51 +0200

    minor changes

--- main.c
+++ main.c
@@ -4,29 +4,29 @@
 
 typedef struct _data_t {
   double x[2];
-  size_t tag;
-  long long int prev_tag;
-  long long int this_tag;
-  long long int next_tag;
+  size_t score;
+  long long int prev_score;
+  long long int this_score;
+  long long int next_score;
 } data_t;
 
 void quickSort(data_t *arr, int left, int right);
 void assignTag(data_t *vector, size_t N);
 
 
-// returns first element with tag >= inputTag
+// returns first element with score >= inputTag
 size_t getPositionOfTag(data_t *vector,
-        size_t N, size_t tag);
+        size_t N, size_t score);
 
 void printRange(data_t *vector, size_t N,
-          size_t first_tag, size_t last_tag) {
-  size_t position = getPositionOfTag(vector, N, first_tag);
+          size_t first_score, size_t last_score) {
+  size_t position = getPositionOfTag(vector, N, first_score);
   if(position == N) return;
-  if(vector[position].tag > last_tag) return;
+  if(vector[position].score > last_score) return;
   do {
     printf("(%f,%f)\n", vector[position].x[0], vector[position].x[1]);
     ++position;
-  } while(vector[position].tag <= last_tag);
+  } while(vector[position].score <= last_score);
 
   return;
 }
@@ -38,12 +38,12 @@ void printBox(data_t *vector, size_t N, size_t nBox,
   size_t minColumn = floor(x0*nBox);
   size_t maxColumn = floor(x1*nBox);
   size_t i;
-  size_t first_tag;
-  size_t last_tag;
+  size_t first_score;
+  size_t last_score;
   for(i=minRow; i<=maxRow; ++i){
-    first_tag = minColumn + i*nBox;
-    last_tag = maxColumn + i*nBox;
-    printRange(vector, N, first_tag, last_tag);
+    first_score = minColumn + i*nBox;
+    last_score = maxColumn + i*nBox;
+    printRange(vector, N, first_score, last_score);
   }
 }
 
@@ -60,7 +60,7 @@ int main(int argc, char const *argv[]) {
     double y = ((double) rand()) / RAND_MAX;
     vector[i].x[0] = x;
     vector[i].x[1] = y;
-    vector[i].tag = (size_t) floor(x*nBox) + nBox*floor(y*nBox);
+    vector[i].score = (size_t) floor(x*nBox) + nBox*floor(y*nBox);
   }
 
   quickSort(vector, 0, N);
@@ -82,9 +82,9 @@ void quickSort(data_t *arr, int left, int right) {
 
   /* partition */
   while(i <= j) {
-    while(arr[i].tag < pivot.tag)
+    while(arr[i].score < pivot.score)
       ++i;
-    while(arr[j].tag > pivot.tag)
+    while(arr[j].score > pivot.score)
       --j;
     if(i <= j) {
       tmp = arr[i];
@@ -105,44 +105,44 @@ void quickSort(data_t *arr, int left, int right) {
 void assignTag(data_t *vector, size_t N) {
   long long int i, j;
 
-  vector[0].prev_tag = -1;
-  vector[0].this_tag = vector[0].tag;
+  vector[0].prev_score = -1;
+  vector[0].this_score = vector[0].score;
   for(i=1; i<N; ++i){
-    if(vector[i].tag != vector[i-1].tag) {
-      vector[i].prev_tag = vector[i-1].this_tag;
-      vector[i].this_tag = i;
-      for(j=i-1; j>=0 && vector[j].tag == vector[i-1].tag; --j)
-        vector[j].next_tag = i;
+    if(vector[i].score != vector[i-1].score) {
+      vector[i].prev_score = vector[i-1].this_score;
+      vector[i].this_score = i;
+      for(j=i-1; j>=0 && vector[j].score == vector[i-1].score; --j)
+        vector[j].next_score = i;
     } else {
-      vector[i].prev_tag = vector[i-1].prev_tag;
-      vector[i].this_tag = vector[i-1].this_tag;
+      vector[i].prev_score = vector[i-1].prev_score;
+      vector[i].this_score = vector[i-1].this_score;
     }
   }
-  for(j=N-1; j>=0 && vector[j].tag == vector[N-1].tag; --j)
-    vector[j].next_tag = N;
+  for(j=N-1; j>=0 && vector[j].score == vector[N-1].score; --j)
+    vector[j].next_score = N;
   return;
 }
 
 size_t getPositionOfTag(data_t *vector,
-        size_t N, size_t tag) {
+        size_t N, size_t score) {
 
   // CHECK LEFT
-  if(vector[0].tag>tag) return 0;
-  if(vector[N-1].tag<tag) return N;
+  if(vector[0].score>score) return 0;
+  if(vector[N-1].score<score) return N;
   size_t left = 0, right = N-1;
   size_t mid = (left + right) / 2;
   do {
-    if(vector[mid].tag == tag)
-      return vector[mid].this_tag;
+    if(vector[mid].score == score)
+      return vector[mid].this_score;
 
-    if(vector[mid].tag>tag)
+    if(vector[mid].score>score)
       right = mid;
     else
       left = mid;
     mid = (left + right) / 2;
   } while (left != mid);
 
-  if(vector[mid].tag == tag) return vector[mid].this_tag;
+  if(vector[mid].score == score) return vector[mid].this_score;
 
-  return vector[right].this_tag;
+  return vector[right].this_score;
 }
