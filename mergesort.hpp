705116ac72e6615706709d8c8881067a859d43ef
Marcos Rodríguez <marcos@unizar.es>
2018-06-11 10:09:15 +0200

    Minor changes and comments in mergeSort

--- mergesort.hpp
+++ mergesort.hpp
@@ -9,19 +9,16 @@
 
 template<class T>
 void merge(std::vector<T> &vec, int64_t left, int64_t mid, int64_t right) {
-    // int64_t i, j, k;
-  int64_t n1 = mid - left;
-  int64_t n2 =  right - mid;
 
-  /* create temp arrs */
+  // split vec[left:right] into two arrays
   std::vector<T> leftArr(vec.begin()+left, vec.begin()+mid);
   std::vector<T> rightArr(vec.begin()+mid, vec.begin()+right);
 
 
-  /* Merge the temp arrs back int64_to vec[l..r]*/
-  int64_t i = 0; // Initial index of first subarr
-  int64_t j = 0; // Initial index of second subarr
-  int64_t k = left; // Initial index of merged subarr
+  /* Merge the temp arrs back into vec[left:right]*/
+  int64_t i = 0; // Initial index of leftArr
+  int64_t j = 0; // Initial index of rightArr
+  int64_t k = left; // Initial index of vec[left:right]
   while (i < (int64_t) leftArr.size() && j < (int64_t) rightArr.size()) {
     if (leftArr[i] < rightArr[j]) {
       vec[k] = leftArr[i];
@@ -32,35 +29,29 @@ void merge(std::vector<T> &vec, int64_t left, int64_t mid, int64_t right) {
     }
     ++k;
   }
-  while (i < n1) {
-        vec[k] = leftArr[i];
-        i++;
-        k++;
-    }
-
-    /* Copy the remaining elements of R[], if there
-       are any */
-    while (j < n2)
-    {
-        vec[k] = rightArr[j];
-        j++;
-        k++;
-    }
+  // Copy the remaining  if there
+  while (i < (int64_t) leftArr.size()) {
+    vec[k] = leftArr[i];
+    ++i;
+    ++k;
+  }
+  while (j < (int64_t) rightArr.size()) {
+    vec[k] = rightArr[j];
+    ++j;
+    ++k;
+  }
 }
 
 template<class T>
 void mergeSort(std::vector<T> &vec, int64_t left, int64_t right) {
   if (left < right-1) {
-
     int64_t mid = left+(right-left)/2;
-
     // Sort first and second halves
     mergeSort(vec, left, mid);
     mergeSort(vec, mid, right);
     merge(vec, left, mid, right);
   }
 }
-
 
 template<class T>
 void mergeSort(std::vector<T> &vec) {
