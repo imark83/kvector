6ae1bf02ea579448009d8a5821d229c9fbb1cb89
Marcos Rodríguez <marcos@imark.es>
2020-06-29 09:02:57 +0200

    generador de figuras añadido

---
+++ tex/pyFigs/plotFig04.py
@@ -0,0 +1,21 @@
+#/usr/bin/python3
+# PLOTS SPEED UP VS RETRIEVED ELEMENTS FOR BOTH
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+test1 = np.loadtxt("uniform.txt")
+test2 = np.loadtxt("tulips.txt")
+
+dim=7
+temp1=test1[(test1[:,0]==dim)]
+temp2=test2[(test2[:,0]==dim)]
+plt.loglog(temp1[::5,1]/1e7, temp1[::5,3]/temp1[::5,2], '--*k')
+plt.loglog(temp2[::5,1]/7990272, temp2[::5,3]/temp2[::5,2], '-ok')
+plt.legend(("Uniform Database","Non Uniform Database"))
+plt.xlim((1e-5,1))
+plt.ylim((1e-2,10))
+plt.xlabel("Proportion of retreived elements")
+plt.ylabel("Relative Speed")
+
+plt.show()
